be1fd67272010c4c19232ec399b8ed22e6e36763
viiniciuspaes <viniciusjosepaes@gmail.com>
2016-11-02 23:41:21 -0300

    Classe arvore criada Insert e insertFix feitos

--- Arvore-vermelho-preta.py
+++ Arvore-vermelho-preta.py
@@ -4,7 +4,9 @@ class NoHash:
         self.anterior = None
         self.proximo = None
         self.pai = None
-        self.cor = None
+        self.cor = "preto"
+
+    # -------------------Gets and Sets--------------------------
 
     def getValor(self):
         return self.valor
@@ -20,3 +22,70 @@ class NoHash:
         return self.pai
     def setPai(self,no):
         self.pai = no
+    def getCor(self):
+        return self.cor
+    def setCor(self,cor):
+        self.cor=cor
+
+
+class ArvoreRB:
+    def __init__(self):
+        self.raiz = None
+        #self.null = NoHash(None) #nao sei se sera necessario criar um ponteiro para um No preto null
+
+    def add(self,no):
+        if self.raiz == None:
+            self.raiz= no
+        else:
+            self.inserir(no)
+
+    def inserir(self, no):
+        auxiliar = None
+        noLocal = self.raiz
+        while noLocal != None:
+            auxiliar = noLocal
+            if no.getValor() < noLocal.getValor():
+                noLocal = noLocal.getAnterior()
+            else:
+                noLocal = noLocal.getProximo()
+        no.setPai(auxiliar)
+        if auxiliar == None:
+            self.raiz=no
+        elif no.getValor() < auxiliar.getValor():
+            auxiliar.setAnterior(no)
+        else:
+            auxiliar.setProximo(no)
+        no.setCor("vermelho")
+        #self.inserirFix(no)
+
+    def inserirFix(self, no):
+        while no.getPai().getCor() == "vermelho":
+            if no.getPai() ==  no.getPai().getPai().getAnterior():
+                auxiliar = no.getPai().getPai().getProximo()
+                if auxiliar.getCor() ==  "vermelho":
+                    no.getPai().setCor("preto")
+                    auxiliar.setCor("preto")
+                    no.getPai().getPai().setCor("vermelho")
+                    no = no.getPai().getPai()
+                else:
+                    if no == no.getPai().getProximo():
+                        no = no.getPai()
+                        #self.rotEsq(no)
+                    no.getPai().setCor("preto")
+                    no.getPai().getPai().setCor("vermelho")
+                    #self.rotDir(no.getPai().getPai())
+            else:
+                auxiliar = no.getPai().getPai().getAnterior()
+                if auxiliar.getCor() == "vermelho":
+                    no.getPai().setCor("preto")
+                    auxiliar.setCor("preto")
+                    no.getPai().getPai().setCor("vermelho")
+                    no = no.getPai().getPai()
+                else:
+                    if auxiliar == auxiliar.getPai().getAnterior():
+                        no = no.getPai()
+                        #self.rotDir(no)
+                    no.getPai().setCor("preto")
+                    no.getPai().getPai().setCor("vermelho")
+                    #self.rotEsq(no.getPai().getPai())
+        self.raiz.setCor("preto")
